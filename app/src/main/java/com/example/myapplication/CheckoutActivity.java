7b5d6e61f9e3298d78bd4f9259c9216edaba1235
IvanPB16 <41024238+IvanPB16@users.noreply.github.com>
2023-09-05 17:43:23 -0600

    modified the code so that it could read the data using the components individually, and not the widget

--- app/src/main/java/com/example/myapplication/CheckoutActivity.java
+++ app/src/main/java/com/example/myapplication/CheckoutActivity.java
@@ -24,6 +24,11 @@ import com.stripe.android.payments.paymentlauncher.PaymentLauncher;
 import com.stripe.android.payments.paymentlauncher.PaymentResult;
 import com.stripe.android.payments.paymentlauncher.PaymentResult;
 import com.stripe.android.view.CardInputWidget;
+import com.stripe.android.view.CardNumberEditText;
+import com.stripe.android.view.ExpiryDateEditText;
+import com.stripe.android.view.CvcEditText;
+import com.stripe.android.view.PostalCodeEditText;
+
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -44,7 +49,7 @@ public class CheckoutActivity extends AppCompatActivity {
         final PaymentConfiguration paymentConfiguration = PaymentConfiguration.getInstance(getApplicationContext());
         paymentLauncher = PaymentLauncher.Companion.create(
                 this,
-                paymentConfiguration.getPublishableKey(),
+                "pk_test_51NAb73HVWg5sdyHq0fzExq14Uxk5K11l7AdXi5Zrhhops4V5EqElJQw1TEDdLU6CC5j3Rjy7dTsH1aV7jMb9S5VF00rrjcpmBj",
                 paymentConfiguration.getStripeAccountId(),
                 this::onPaymentResult
         );
@@ -55,7 +60,33 @@ public class CheckoutActivity extends AppCompatActivity {
         Button payButton = findViewById(R.id.payButton);
         payButton.setOnClickListener((View view) -> {
             // Collect card details
-            CardInputWidget cardInputWidget = findViewById(R.id.cardInputWidget);
+            //CardInputWidget cardInputWidget = findViewById(R.id.cardInputWidget);
+            //Log.d("Prueba card cardInputWidget", "cardInputWidget " + cardInputWidget);
+            //PaymentMethodCreateParams.Card card = cardInputWidget.getPaymentMethodCard();
+            //Log.d("Prueba card", "card " + card);
+
+
+            CardNumberEditText cardInput = findViewById(R.id.cardNumberEditText);
+            Log.d("Prueba cardInput", "cardInput " + cardInput.getCardBrand());
+            Log.d("Prueba cardInput", "cardInput getText " + cardInput.getText());
+            ExpiryDateEditText expiryDateInput = findViewById(R.id.expiryDateEditText);
+            String[] parts = expiryDateInput.getText().toString().split("/");
+            Log.d("Prueba cardInput", "cardInput getText " + expiryDateInput.getText().toString());
+            CvcEditText cvcInput = findViewById(R.id.cvcEditText);
+            Log.d("Prueba cardInput", "cardInput getText " + cvcInput.getText().toString());
+            PostalCodeEditText zipcodeInput = findViewById(R.id.zipCode);
+            Log.d("Prueba cardInput", "cardInput getText " + zipcodeInput.getText().toString());
+            /*
+            EditText experireInput = findViewById(R.id.expiryDateEditText);
+            Log.d("Prueba experireInput", "experireInput " + experireInput.toString());
+            EditText cvcInput = findViewById(R.id.cvcEditText);
+            Log.d("Prueba cvcInput", "cvcInput " + cvcInput.toString());
+            */
+            CardInputWidget cardInputWidget = new CardInputWidget(this);
+            cardInputWidget.setCardNumber(cardInput.getText().toString());
+            cardInputWidget.setExpiryDate( Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+            cardInputWidget.setCvcCode(cvcInput.getText().toString());
+
             PaymentMethodCreateParams.Card card = cardInputWidget.getPaymentMethodCard();
             Log.d("Prueba card", "card " + card);
 
@@ -63,9 +94,9 @@ public class CheckoutActivity extends AppCompatActivity {
             // This example collects the customer's email to know which customer the PaymentMethod belongs to, but your app might use an account id, session cookie, etc.
             EditText nameInput = findViewById(R.id.nameInput);
             Log.d("Prueba card emailInput", "emailInput " + nameInput);
-            PaymentMethod.BillingDetails billingDetails = (new PaymentMethod.BillingDetails.Builder())
-                    .setName(nameInput.getText().toString())
-                    .build();
+
+
+            PaymentMethod.BillingDetails billingDetails = (new PaymentMethod.BillingDetails.Builder()).setName(nameInput.getText().toString()).build();
             Log.d("Prueba card billingDetails", "billingDetails " + billingDetails);
             if (card != null) {
                 Log.d("Prueba if card", "card " + card);
@@ -73,7 +104,7 @@ public class CheckoutActivity extends AppCompatActivity {
                 PaymentMethodCreateParams paymentMethodParams = PaymentMethodCreateParams
                         .create(card, billingDetails);
                 Log.d("Prueba card paymentMethodParams", "paymentMethodParams " + paymentMethodParams.toString());
-                ConfirmSetupIntentParams confirmParams = ConfirmSetupIntentParams.create(paymentMethodParams, "seti_1NlEFdHVWg5sdyHqCvHr8yrs_secret_OYKvDC1jJPfxk9BRP964OSGtIbyYIHw");
+                ConfirmSetupIntentParams confirmParams = ConfirmSetupIntentParams.create(paymentMethodParams, "clientSecret");
                 Log.d("Prueba confirmParams", "confirmParams " + confirmParams);
                 paymentLauncher.confirm(confirmParams);
             }
@@ -98,13 +129,13 @@ public class CheckoutActivity extends AppCompatActivity {
 
     private void displayAlert(String title, String message, Boolean restartDemo) {
         runOnUiThread(()-> {
-            final CardInputWidget cardInputWidget= findViewById(R.id.cardInputWidget);
+            //final CardInputWidget cardInputWidget= findViewById(R.id.cardNumberEditText);
             final AlertDialog.Builder builder = new AlertDialog.Builder(this)
                     .setTitle(title)
                     .setMessage(message);
             if (restartDemo) {
                 builder.setPositiveButton("Restart Demo", (v1, v2) -> {
-                    cardInputWidget.clear();
+                   // cardInputWidget.clear();
                     loadPage();
                 });
             } else {
